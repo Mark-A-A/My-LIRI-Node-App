49e3a201cd0ffe48b643a778a04627f4c140efdd
Mark-A-A <markawai01@gmail.com>
2016-01-18 18:24:15 -0500

    npm init. package.json created. Working on installing packages

--- js/liri-Twitter.js
+++ js/liri-Twitter.js
@@ -1,1 +1,15 @@
+var Twitter = require('twitter');
+
 var keys = require("./keys.js");
+var client = new Twitter({keys.twitterKeys});
+
+var paramsScreenName = process.argv(3);
+var params = {screen_name: 'paramsScreenName'} ;
+
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (error){
+    console.log(error);
+  }else if (!error) {
+    console.log(tweets);
+  }
+});
