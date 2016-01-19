b731b2bae4c08b0c83f0c654e8a5472fb0008834
Mark-A-A <markawai01@gmail.com>
2016-01-19 17:49:09 -0500

    Accessing data using Spotify package. Building function to show data

--- liri-Spotify.js
+++ liri-Spotify.js
@@ -1,7 +1,13 @@
-module.exports = function() {
+exports.searchSpotify = function() {
+  debugger
   var spotify = require('spotify');
   var searchQuery = process.argv[3]
  
+  var artistName= "";
+  var songName = "";
+  var albumName ="";
+  var previewLink ="";
+
   spotify.search({ type: 'track', query: searchQuery }, function(err, data) {
       if ( err ) {
           console.log('Error occurred: ' + err);
@@ -10,7 +16,13 @@ module.exports = function() {
    
       // Do something with 'data'
       console.log(data);
+      console.log(data.tracks.items[0].artists[0].name);    //artist name
+      console.log(data.tracks.items[0].name);               //song name
+      console.log(data.tracks.items[0].preview_url);        //preview link
+      console.log(data.tracks.items[0].album.name);         // Album name
       return data;
+
+
   });
 }
 
