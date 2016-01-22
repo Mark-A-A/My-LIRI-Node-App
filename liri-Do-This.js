6eb16e8dac1b8bce6bab3498812538a77f4a10ed
Mark-A-A <markawai01@gmail.com>
2016-01-21 20:12:00 -0500

    Do-This Working

--- liri-Do-This.js
+++ liri-Do-This.js
@@ -16,11 +16,14 @@ exports.doThis = function() {
 
     console.log(data);                                   
 
-    var parameters = data.split(", ");
+    var parameters = data.split(",");
     console.log(parameters);
 
     var parameter1 = parameters[0];
+    console.log(parameter1);
+
     var parameter2 = parameters[1];
+    console.log(parameter2);
 
     liriMain.taskDoThis(parameter1, parameter2);
 
